fcd2acb2cbc0f4d76e37ea4b83df0727215ba0d9
Leszek Pietrzak <smutnyleszek@gmail.com>
2017-03-15 23:20:24 +0100

    actual testData.js

---
+++ test/testData.js
@@ -0,0 +1,20 @@
+angular.module('testAppModule').constant('testData', {
+    hotels: {
+        Establishments: [
+            {
+                Distance: 1024.768,
+                EstablishmentId: 123456,
+                EstablishmentType: 'Hotel',
+                Location: 'Warsaw',
+                MinCost: 499,
+                Name: 'Test Hotel',
+                Stars: 4,
+                UserRating: 8,
+                UserRatingTitle: 'Great',
+                UserRatingCount: 999,
+                ImageUrl: 'https://i.t-rp.co.uk/ei/3/7/4/8/4/8/0_400_400.jpg',
+                ThumbnailUrl: 'https://i.t-rp.co.uk/ei/3/7/4/8/4/8/0_80_80.jpg'
+            }
+        ]
+    }
+});
